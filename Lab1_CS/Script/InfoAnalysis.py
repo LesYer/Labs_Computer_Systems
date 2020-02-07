64c6d4ba139bddc28570b3a73eecca2ca5ef0d7d
Oleksii Yermolin <lexa.yermolin@gmail.com>
2020-02-07 02:50:23 +0200

    07.02.2020 02:50

--- Lab1_CS/Script/InfoAnalysis.py
+++ Lab1_CS/Script/InfoAnalysis.py
@@ -25,7 +25,6 @@ class Analysis():
     def char_frequency(self):
         chars = list(set(self.read_file()))
         collection_chars = {}
-        count = 0
         for item in chars:
             collection_chars[item] = 0
         for item in self.read_file():
@@ -34,6 +33,14 @@ class Analysis():
             collection_chars[item] /= len(self.read_file())
         return collection_chars
 
+    def calculate_chars(self):
+        import re
+        count = 0
+        for item in self.read_file():
+            if item in '([а-яА-Я])':
+                count += 1
+        return count * self.entropy(self.read_file())
+
 
 
 file1 = "G:\Projects\Labs_Computer_Systems\Lab1_CS\TextFiles\Whenidie.txt"
@@ -41,5 +48,6 @@ file2 = "G:\Projects\Labs_Computer_Systems\Lab1_CS\TextFiles\Tigrolovi.txt"
 file3 = "G:\Projects\Labs_Computer_Systems\Lab1_CS\TextFiles\Aforyzmy.txt"
 
 analysis = Analysis(file1)
-print(analysis.entropy(analysis.read_file()))
-print(analysis.char_frequency())
+print('Entrophy:', analysis.entropy(analysis.read_file()))
+print('Char frequency:', analysis.char_frequency())
+print('Calculate chars', analysis.calculate_chars())
